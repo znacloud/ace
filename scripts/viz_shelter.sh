ee1400dd83751f73d3588a47e9aa24f38196b382
stephanzeng <znacloud@126.com>
2024-08-17 18:45:15 +0800

    fix errors

--- scripts/viz_shelter.sh
+++ scripts/viz_shelter.sh
@@ -11,13 +11,16 @@ testing_exe="${REPO_PATH}/test_ace.py"
 
 datasets_folder="${REPO_PATH}/datasets"
 out_dir="${REPO_PATH}/output/viz_maps/shelter"
-renderings_dir="${REPO_PATH}/output/renderings/shelter"
+renderings_train_dir="${REPO_PATH}/output/renderings/shelter_train"
+renderings_test_dir="${REPO_PATH}/output/renderings/shelter_test"
 
 mkdir -p "$out_dir"
-mkdir -p "renderings_dir"
+mkdir -p "$renderings_train_dir"
+mkdir -p "$renderings_test_dir"
 
 for scene in ${scenes[*]}; do
-  python $training_exe "$datasets_folder/$scene" "$out_dir/$scene.pt" --render_visualization True --render_target_path "$renderings_dir" --render_camera_z_offset 2
-  python $testing_exe "$datasets_folder/$scene" "$out_dir/$scene.pt" --render_visualization True  --render_target_path "$renderings_dir" --render_pose_error_threshold 10 --render_frame_skip 3
-  /usr/bin/ffmpeg -framerate 30 -pattern_type glob -i "$renderings_dir/$scene/*.png" -c:v libx264 -pix_fmt yuv420p "$renderings_dir/$scene.mp4"
+  python $training_exe "$datasets_folder/$scene" "$out_dir/$scene.pt" --render_visualization True --render_target_path "$renderings_train_dir"
+  ffmpeg -framerate 30 -pattern_type glob -i "$renderings_train_dir/$scene/*.png" -c:v libx264 -pix_fmt yuv420p "$renderings_train_dir/$scene.mp4"
+  python $testing_exe "$datasets_folder/$scene" "$out_dir/$scene.pt" --render_visualization True  --render_target_path "$renderings_test_dir"
+  ffmpeg -framerate 30 -pattern_type glob -i "$renderings_test_dir/$scene/*.png" -c:v libx264 -pix_fmt yuv420p "$renderings_test_dir/$scene.mp4"
 done
